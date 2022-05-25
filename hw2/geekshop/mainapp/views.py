eef7f136a2a93501aed3dce83d0ff7f624c7761e
AlexProsku <pros1217@yandex.ru>
2022-05-26 02:01:26 +0300

    HW_2 done_2

--- hw2/geekshop/mainapp/views.py
+++ hw2/geekshop/mainapp/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,7 +8,25 @@ def main(request):
     return render(request, 'mainapp/index.html')
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_all', 'title': 'все'},
+        {'href': 'products_home', 'title': 'дом'},
+        {'href': 'products_office', 'title': 'офис'},
+        {'href': 'products_modern', 'title': 'модерн'},
+        {'href': 'products_classic', 'title': 'классика'},
+    ]
+
+    context = {
+        'links_menu': links_menu
+    }
+
+    return render(request, 'mainapp/products.html', context)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    with open('mainapp/contacts_file.json', 'r', encoding='utf-8') as file:
+        contacts_file = json.load(file)
+
+    context = {
+        'contacts_file': contacts_file
+    }
+    return render(request, 'mainapp/contact.html', context)
